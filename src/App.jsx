28a94c9d6dcb5195e29135b0dee7c6584b330b26
Majok-Bol <bolmajok001@gmail.com>
2025-08-04 10:16:47 +0300

    added more color pick options

--- src/App.jsx
+++ src/App.jsx
@@ -1,56 +1,20 @@
-//import useState from react
-
 import { useState } from "react";
-//pick colors at random
-let count = 1;
-let counter = 0;
-// import{useState} from"react";
-function App() {
-  let colors = [
-    "#f3f3f3",
-    "#DE4D3A",
-    "#60DE3A",
-    "#3ADEC8",
-    "#3A9FDE",
-    "#3A4DDE",
-    "#D03ADE",
-    "#DE3A76",
-    "#ADDE3A",
-    "#DE5E3A",
-    "#DEA53A",
-  ];
-  console.log("First color: ", colors[0]);
-  let pick = colors[counter];
-  console.log("Color picked: ", pick);
-  let endingIndex = colors.length;
-  const [initialColor, setColor] = useState(pick);
-  function handleButtonClick() {
-    setColor(initialColor);
-    if (count <endingIndex) {
-      console.log("Counter: ", count);
-      let initialPick = colors[counter];
-      setColor(initialPick);
-      counter = counter + 1;
-      if(counter===colors.length){
-        alert("Reached max")
-      }
-    }
-    console.log("Count: ", counter);
 
-    count = count + 1;
-
-  }
+// import{useState} from 'react';
+export default function App() {
+  //initial color is white
+  const [colour, setColor] = useState("#ffffff");
   return (
     <>
-      <button id="btn" onClick={handleButtonClick}>
-        Click Me
-      </button>
-      <div id="container" style={{ backgroundColor:colors[counter] }}>
-        Text
+    <p>Color picker built with React</p>
+
+      <div id="color-container" style={{ backgroundColor: colour }}>
+        <input
+          type="color"
+          value={colour}
+          onChange={(e) => setColor(e.target.value)}
+        />
       </div>
     </>
   );
-
-  //
 }
-export default App;
